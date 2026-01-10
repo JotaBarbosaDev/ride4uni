6de4eb649322c11df73d357f61238733c96cc4f5
Maruqes <m4rques1s0n@gmail.com>
2026-01-10 19:04:04 +0000

    n digo nada

--- api/api.js
+++ api/api.js
@@ -6,3 +6,15 @@ export const api = axios.create({
   baseURL: "http://localhost:3000/api",
 });
 
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const status = error?.response?.status;
+    if (status === 401 && typeof window !== "undefined") {
+      if (window.location.pathname !== "/login") {
+        window.location.assign("/login");
+      }
+    }
+    return Promise.reject(error);
+  }
+);
